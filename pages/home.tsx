800931a3a6de8477aecc5448ac8a553eb749cd16
tnsgud <qkrtnsgud0229@gmail.com>
2022-02-13 22:24:02 +0900

    profile photo에 대한 조건 추가

--- pages/home.tsx
+++ pages/home.tsx
@@ -44,7 +44,7 @@ const Home = ({ uid }: { uid: string }) => {
         <img
           className='rounded-2xl w-36 h-36 mx-5'
           src={
-            userInfo.photo === null || photo === ''
+            userInfo.photo === null || userInfo.photo === ''
               ? 'https://w.namu.la/s/40de86374ddd74756b31d4694a7434ee9398baa51fa5ae72d28f2eeeafdadf0c475c55c58e29a684920e0d6a42602b339f8aaf6d19764b04405a0f8bee7f598d2922db9475579419aac4635d0a71fdb8a4b2343cb550e6ed93e13c1a05cede75'
               : userInfo.photo
           }
