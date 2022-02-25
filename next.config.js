4dc57ac4fb4a6dc70886753e05ca520baef08ad4
tnsgud <qkrtnsgud0229@gmail.com>
2022-02-25 15:49:24 +0900

    cors 에러 해결 못함

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        destination: `${process.env.NEXT_PUBLIC_PREFIX_DEV}/:path*`,
-        source: '/',
-      },
-    ];
-  },
+  crossOrigin:'anonymous',
   reactStrictMode: true,
 };
