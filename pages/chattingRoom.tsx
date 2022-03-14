36221d2f1ce52f17eaec54923a3bca4e1a382bce
tnsgud <qkrtnsgud0229@gmail.com>
2022-03-14 22:04:44 +0900

    채팅내역 구현 및 약간의 스타일 작업

--- pages/chattingRoom.tsx
+++ pages/chattingRoom.tsx
@@ -14,75 +14,37 @@ type ChattingDataWithName = ChattingData & { name: string };
 type SocketChattingData = ChattingData & { roomId: string; type: string };
 
 const socket = io(`${process.env.NEXT_PUBLIC_PREFIX_DEV}/chattingRoom`, {
-  transports: ['polling'],
+  transports: ['polling']
 });
 
 const ChattingRoom = () => {
   const [chattingData, setChattingData] = useState<ChattingDataWithName[]>([]);
   const [content, setContent] = useState<string>('');
-  let displayChattingData: Array<ChattingDataWithName> = [];
 
   const getChattingData = async () => {
     const res = await axios.get(
       `${process.env.NEXT_PUBLIC_PREFIX_DEV}/chatting/getChattingData/간럿섬외능낌킵븐앳빙띳쌜`
     );
     const data = res.data;
-    await Promise.all(
-      data.map(async (id: ChattingData) => {
-        const nameRes = await axios.get(
-          `${process.env.NEXT_PUBLIC_PREFIX_DEV}/user/getName/${id.sender}`
-        );
-        const name = nameRes.data.displayUserName;
-        displayChattingData.push({
-          ...id,
-          name,
-        });
-      })
-    );
-
-    setChattingData(displayChattingData);
+    setChattingData(data);
   };
 
   useEffect(() => {
     getChattingData();
     socket.emit('enter', {
       uid: getCookie('uid'),
-      roomId: '간럿섬외능낌킵븐앳빙띳쌜',
+      roomId: '간럿섬외능낌킵븐앳빙띳쌜'
     });
   }, []);
 
   useEffect(() => {
-    displayChattingData.map((data, i) => {
-      return (
-        <div key={i}>
-          {data.name} - {data.content}
-        </div>
-      );
-    });
-  }, [displayChattingData]);
-
-  useEffect(() => {
     socket.on('listen', (data: SocketChattingData) => {
-      console.log(data);
-
-      displayChattingData.push({
-        content: data.content,
-        dateTime: data.dateTime,
-        name: data.sender,
+      setChattingData([...chattingData, {
+        name:'',
         sender: data.sender,
-      });
-
-      // chatting
-
-      // setChattingData([
-      //   ...chattingData,
-      //   {
-      // content: data.content,
-      // dateTime: data.dateTime,
-      // name: data.sender,
-      // sender: data.sender,
-      //   },
-      // ]);
+        content: data.content,
+        dateTime: data.dateTime
+      }]);
     });
   });
 
@@ -92,9 +54,10 @@ const ChattingRoom = () => {
       sender: getCookie('uid'),
       type: 'msg',
       content: content,
-      dateTime: '2022-02-25 23:32:00',
+      dateTime: '2022-02-25 23:32:00'
     });
     setContent('');
+
   };
 
   const onChange = (e: FormEvent<HTMLInputElement>) => {
@@ -104,16 +67,18 @@ const ChattingRoom = () => {
   return (
     <>
       <div>
-        {displayChattingData.map((data, i) => {
-          return (
-            <div key={i}>
-              {data.name} - {data.content}
-            </div>
-          );
-        })}
+        {
+          chattingData.map((data, index) => {
+            const isMe = data.sender === '갠늄않론뫄뫈캥얌매쑹박왕';
+            return <div key={index} className={`${isMe ? 'flex justify-end' : 'flex justify-start'} p-2`}>
+              <div className={`${isMe ? 'bg-blue-200 ':'bg-blue-400'} text-center w-fit p-5 rounded-xl`}>{data.content}</div>
+            </div>;
+          })
+        }
       </div>
       <div>
         <input
+          value={content}
           className='outline outline-offset-2 outline-1'
           onChange={onChange}
         />
